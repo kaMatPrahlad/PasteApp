08e016e8869bc726047ea2215bd0191e60ec8fe3
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-02-22 18:57:10 +0530

    Implement Home and Navbar components with paste creation and updating functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { updatesToPastes } from "../features/counter/counterSlice";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pasteId = searchParams.get("pasteId");
+
+  function createPaste() {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId || Date.now().toString(36),
+      createdAt: new Date().toISOString(),
+    };
+
+    if (pasteId) {
+      //Update
+      dispatch(updatesToPastes(paste));
+    } else {
+      //Create
+      dispatch(addToPastes(paste));
+    }
+    //After Creation or Updation
+    setTitle("");
+    setValue("");
+    setSearchParams({});
+  }
+
+  return (
+    <div>
+      <div className="flex flex-row gap-7 place-content-between">
+        <input
+          className="p-1 rounded-2xl mt-2 w-[55%] pl-4"
+          id="search"
+          type="text"
+          placeholder="Enter title here"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <button onClick={createPaste} className="p-2 rounded-2xl mt-2">
+          {pasteId ? "Update My Paste" : "Create My Paste"}
+        </button>
+      </div>
+      <div className="mt-8">
+        <textarea
+          className="rounded-2xl mt-4 min-w-125 p-4"
+          id="input"
+          value={value}
+          placeholder="Enter content here"
+          onChange={(e) => setValue(e.target.value)}
+          rows={20}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
